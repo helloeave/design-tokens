f599a4d810c75f7a737daa1222d85eb2304881ba
Shawn Patrick Rice <rice@shawnrice.org>
2018-11-07 11:13:53 -0500

    update teikn to 0.1.3; add comments

--- generate.js
+++ generate.js
@@ -1,5 +1,14 @@
 const { TokenWriter } = require('teikn');
-// const { TokenWriter } = require('../teikn');
 const tokens = require('./raw-tokens');
 
-new TokenWriter({ outDir: __dirname, generators: ['mapScss', 'json', 'es'] }).run(tokens);
+new TokenWriter({
+  // Generate the files to this directory. It allows us to import things like
+  // `import '~@eave/design-tokens/tokens';` in scss and
+  // `import tokens from '@eave/design-tokens/tokens';` in javascript
+  outDir: __dirname,
+  // We're going to generate
+  //   - the es modules JS version
+  //   - the map w/ getter version of the scss
+  //   - the json representation of the tokens
+  generators: ['es', 'mapScss', 'json'],
+}).run(tokens);
